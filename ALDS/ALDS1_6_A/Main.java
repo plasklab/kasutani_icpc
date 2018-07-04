8c1da7a21589bd4eed077451b5e86ca77b860dff
ayato1995 <ayato.k070206@outlook.jp>
2018-07-04 11:01:58 +0900

    ALDS1_6_Aの完成版を追加

--- ALDS/ALDS1_6_A/Main.java
+++ ALDS/ALDS1_6_A/Main.java
@@ -1,14 +1,12 @@
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.lang.*;
 
 class Main {
 	int n;
 	int[] a;
 	int[] b;
-	int[] c = new int[10000 + 1];
 	public static void main(String[] args) {
-		new Main().run3();
+		new Main().run();
 	}
 
 	void run() {
@@ -19,58 +17,14 @@ class Main {
 		int k = 0;
 
 		for (int i = 0; i < n; i++) {
-			if (scan.hasNextInt())
-				a[i] = scan.nextInt();
+			if (scan.hasNext())
+				a[i] = Integer.parseInt(scan.next());
 			if (k < a[i]) k = a[i];
 		}
 
 		countingSort(k);
-		printArray(b);
+		System.out.println(builderArray(b));
 
-	}
-
-	void run2() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			n = Integer.parseInt(br.readLine());
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		a = new int[n];
-		b = new int[n];
-		int k = 0;
-		String str = null;
-		try {
-			str = br.readLine();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		String[] str2 = str.split(" ", 0);
-		for (int i = 0; i < n; i++) {
-			a[i] = Integer.parseInt(str2[i]);
-			if (k < a[i]) k = a[i];
-		}
-
-		countingSort(k);
-		printArray(b);
-	}
-
-	void run3() {
-		Scanner scan = new Scanner(System.in);
-		n = scan.nextInt();
-		a = new int[n];
-		b = new int[n];
-		int k = 0;
-
-		for (int i = 0; i < n; i++) {
-			if (scan.hasNextInt()) {
-				a[i] = scan.nextInt();
-				c[a[i]]++;
-				if (k < a[i]) k = a[i];
-			}
-		}
-		countingSort2(k);
-		printArray(b);
 	}
 
 	void countingSort(int k) {
@@ -93,22 +47,12 @@ class Main {
 		}
 	}
 
-	void countingSort2(int k) {
-		for (int i = 1; i <= k; i++) {
-			c[i] = c[i] + c[i - 1];
+	String builderArray(int[] b) {
+		StringBuilder str = new StringBuilder();
+		str.append(b[0]);
+		for (int i = 1; i < b.length; i++) {
+			str.append(" " + b[i]);
 		}
-		for (int i = n - 1; i >= 0; i--) {
-			b[c[a[i]] - 1] = a[i];
-			c[a[i]]--;
-		}
-	}
-
-	void printArray(int[] b) {
-		int i = 0;
-		System.out.print(b[i]);
-		for (i = 1; i < b.length; i++) {
-			System.out.print(" " + b[i]);
-		}
-		System.out.println();
+		return str.toString();
 	}
 }
