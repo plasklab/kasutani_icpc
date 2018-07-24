1aa1d668a4157cf1529c67d8b3d0c818c3a685c4
ayato1995 <ayato.k070206@outlook.jp>
2018-07-24 14:51:23 +0900

    1_6_Dの追加

--- ALDS/ALDS1_6_D/Main.java
+++ ALDS/ALDS1_6_D/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Main {
 	int minCost = 0;
+	int mv;
 
 	public static void main(String[] args) {
 		new Main().run();
@@ -11,28 +12,56 @@ class Main {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int[] w = new int[n];
+		int[] sort = new int[n];
+		boolean[] flag = new boolean[n];
 
 		for (int i = 0; i < n; i++) {
-			w[i] = scan.nextInt();
+			sort[i] = scan.nextInt();
+			w[i] = sort[i];
+			flag[i] = true;
 		}
 
-		selectionSort(w);
+		mv = w[0];
+		for (int i = 1; i < w.length; i++) {
+			if (mv > w[i]) mv = w[i];
+		}
+
+		Arrays.sort(sort);
+		createCircle(w, sort, flag);
 		System.out.println(minCost);
 	}
 
-	void selectionSort(int[] w) {
+	void createCircle (int[] w, int[] sort, boolean[] flag) {
 		for (int i = 0; i < w.length; i++) {
-			int minj = i;
-			for (int j = i; j < w.length; j++) {
-				if (w[minj] > w[j])
-					minj = j;
+			int min = 0;
+			if (!flag[i]) continue;
+			flag[i] = false;
+			int tmp = w[i];
+			int minv = tmp;
+			int cnt = 1;
+			while (true) {
+				int index = i;
+				for (int j = 0; j < sort.length; j++) {
+					if (tmp == sort[j] && i != j) {
+						min += tmp;
+						tmp = w[j];
+						if (minv > tmp) minv = tmp;
+						index = j;
+						cnt++;
+						break;
+					}
+				}
+				if (!flag[index]) {
+					flag[index] = false;
+					break;
+				}
+				flag[index] = false;
 			}
-			if (i != minj) {
-				int swp = w[i];
-				w[i] = w[minj];
-				w[minj] = swp;
-				minCost += w[i] + w[minj];
-			}
+			min += tmp;
+			int m = min + minv + (cnt + 1) * mv;
+			min += (cnt - 2) * minv;
+			if (min > m) min = m;
+			minCost += min;
 		}
 	}
 }
